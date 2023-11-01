d8f63d2d81fea1f9c7367241d0204967c774a0ef
RONALD ALEXIS CUADROS BENAVIDES <ronaldcuadros.197@gmail.com>
2023-11-01 12:30:31 -0500

    cambios de comprobación

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -4,22 +4,27 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent {
-  usuario: { tipoDocumento: string, numeroDocumento: string } = {
+  usuario: { tipoDocumento: string; numeroDocumento: string } = {
     tipoDocumento: '',
     numeroDocumento: '',
   };
 
   habilitarBoton = false;
 
-  constructor (private router: Router) {
-  }
+  constructor(private router: Router) {}
 
   updateCadenaConSeparadores(): void {
-    this.usuario.numeroDocumento = this.usuario.numeroDocumento.replace(/\D/g, '');
-    this.usuario.numeroDocumento = this.formatWithCommas(this.usuario.numeroDocumento);
+    this.usuario.numeroDocumento = this.usuario.numeroDocumento.replace(
+      /\D/g,
+      ''
+    );
+    this.usuario.numeroDocumento = this.formatWithCommas(
+      this.usuario.numeroDocumento
+    );
+    this.validateOnChange();
   }
 
   formatWithCommas(value: string): string {
@@ -27,11 +32,33 @@ export class RegistroComponent {
   }
 
   validateOnChange() {
-    const numeroDocumentoSinComas = this.usuario.numeroDocumento.replace(/,/g, '');
-    this.habilitarBoton = numeroDocumentoSinComas.length >= 8 && numeroDocumentoSinComas.length <= 11 && this.usuario.tipoDocumento !== '';
+    const numeroDocumentoSinComas = this.usuario.numeroDocumento.replace(
+      /,/g,
+      ''
+    );
+    const isNumber = /^\d+$/.test(numeroDocumentoSinComas);
+    this.habilitarBoton =
+      isNumber &&
+      numeroDocumentoSinComas.length >= 8 &&
+      numeroDocumentoSinComas.length <= 11 &&
+      this.usuario.tipoDocumento !== '';
+  }
+
+  onKeyPress(event: KeyboardEvent): void {
+    // Obtiene el código de la tecla presionada
+    const keyCode = event.keyCode;
+
+    // Permite solo números (códigos de tecla 48-57 son números 0-9)
+    if (keyCode < 48 || keyCode > 57) {
+      event.preventDefault(); // Evita que se ingrese otro carácter
+    }
   }
 
   onSubmit() {
-    this.router.navigate([`visualizar-datos/${this.usuario.tipoDocumento}/${this.usuario.numeroDocumento.replace(/,/g, '')}`]);
+    this.router.navigate([
+      `visualizar-datos/${
+        this.usuario.tipoDocumento
+      }/${this.usuario.numeroDocumento.replace(/,/g, '')}`,
+    ]);
   }
 }
